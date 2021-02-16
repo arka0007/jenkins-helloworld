e9f70919dd8c570688154b96f6e43f8c4d1eb916
arka0007 <mandalarka77@gmail.com>
2021-02-16 12:57:49 +0530

    add hello world program

--- HelloProgram.java
+++ HelloProgram.java
@@ -1,9 +1,9 @@
 public class HelloProgram {
     public static void main(String[] s)
     {
-        System.out.println("Loop based execution");
+        System.out.println("Loop based execution - CICD Demo");
 
-        for(int i=0;i<10;i++)
+        for(int i=0;i<50;i++)
             System.out.println(i);
     }
 }
